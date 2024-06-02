385c799d72475709d456871aab3e6fa2e85c8746
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-06-02 12:24:06 +1000

    invoice to

--- src/constants/common-constants.ts
+++ src/constants/common-constants.ts
@@ -4,5 +4,5 @@ export const TFJ_MASTER_ADDRESS = Address.parse(
   "EQDRw-Z8l441NCTk2i5ByW4LI6iJChRraxn7z-Clh_MDkpWV"
 );
 export const INVOICE_WALLET_ADDRESS = Address.parse(
-  "0QD-SuoCHsCL2pIZfE8IAKsjc0aDpDUQAoo-ALHl2mje04A-"
+  "UQBondcvD2_aOFADXSWJHs4ZazQDuEl9_wNvGGPxI8hGuLyR"
 );
